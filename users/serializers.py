5f28b90c41ecbe12e544080db0ca606404275cc9
Safian Iddrisu <safiannsterlin@gmail.com>
2024-10-17 19:04:37 +0100

    added serializers file

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Product, Category, Review, Wishlist
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'product', 'user', 'rating', 'comment', 'created_at']
+        read_only_fields = ['user', 'created_at'] 
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wishlist
+        fields = ['id', 'user', 'products', 'created_at']
+        read_only_fields = ['user', 'created_at']
